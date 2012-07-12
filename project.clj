7afe145e0f24b695078c6389ac5bc859336b14d0
Zane Shelby <zaneshelby@gmail.com>
2012-07-12 13:36:04 -0500

    Add missing dependencies.

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject apc-support-bot "0.1.0-SNAPSHOT"
   :description "Get help wearing your ultra-tight A.P.C. jeans."
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [twitter-api "0.6.10"]])
+                 [twitter-api "0.6.10"]
+                 [useful "0.8.0"]
+                 [clojure-opennlp "0.1.10"]]
+  :main apc-support-bot.core)
